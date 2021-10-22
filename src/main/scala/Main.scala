66121781e8ae965e88e4c57f3ae9d9c7d46189b1
nikolaiser <nickolayser@gmail.com>
2021-10-22 14:45:32 +0200

    foo

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -42,7 +42,7 @@ object Main extends IOApp{
       routes = Http4sServerInterpreter[IO].toRoutes(serverEndpoints)
       _ <- BlazeServerBuilder[IO](ec)
         .withHttpApp(Router("/" -> routes, "/api" -> swagger.routes[IO]).orNotFound)
-        .bindHttp(config.serverPort, "localhost")
+        .bindLocal(config.serverPort)
         .serve
         .compile
         .drain
