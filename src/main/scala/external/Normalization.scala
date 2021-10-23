35edbe268c83ccda576a631e810245d5e3d128c2
nikolaiser <nickolayser@gmail.com>
2021-10-23 16:25:08 +0200

    a

--- src/main/scala/external/Normalization.scala
+++ src/main/scala/external/Normalization.scala
@@ -1,5 +1,5 @@
 package external
-import cats.Monad
+import cats.{Id, Monad}
 import common.Config
 import db.models.{Submission, UserSubmission}
 import db.repository.{MetadataStorage, SubmissionStorage}
@@ -7,16 +7,20 @@ import external.models.NormalizationResult
 import sttp.client3._
 import sttp.client3.circe._
 import sttp.model.Uri
+import tofu.logging.Logging
 import tofu.syntax.feither._
 import tofu.syntax.fire._
 import tofu.syntax.foption._
 import tofu.syntax.monadic._
+import tofu.syntax.logging._
 import tofu.{Fire, Raise}
 
-final class Normalization[F[_]: Monad: MetadataStorage: SubmissionStorage: Raise[*[_], Throwable]: Fire](
+final class Normalization[F[_]: Monad: MetadataStorage: SubmissionStorage: Raise[*[_], Throwable]: Fire: Logging.Make](
     sttpBackend: SttpBackend[F, Any],
     config: Config
 ) {
+
+  implicit val log: Logging[F] = Logging.Make[F].forService[Normalization[F]]
 
   val baseUri: Uri = Uri.apply(config.normalization.host, config.normalization.port)
 
@@ -27,6 +31,9 @@ final class Normalization[F[_]: Monad: MetadataStorage: SubmissionStorage: Raise
       .response(asJson[NormalizationResult])
       .send(sttpBackend)
       .map(_.body)
+        .leftMapF{ err =>
+            error"${err.getMessage}".map(_ => err)
+        }
       .reRaise
 
   def normalizeAndSave(photoId: Int): F[Unit] =
