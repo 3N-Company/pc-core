e39d5033826699a4747276cfae5e8fcbfdd08c04
nikolaiser <nickolayser@gmail.com>
2021-10-23 20:52:47 +0200

    user score

--- src/main/scala/db/PhotoInit.scala
+++ src/main/scala/db/PhotoInit.scala
@@ -5,11 +5,13 @@ import cats.syntax.traverse._
 import common.Config
 import db.repository.PhotoStorage
 import external.Colorization
+import tofu.Fire
 import tofu.syntax.monadic._
+import tofu.syntax.fire._
 
 import java.nio.file.Path
 
-final class PhotoInit[F[_]: Sync: ContextShift: PhotoStorage: Colorization](blocker: Blocker, config: Config) {
+final class PhotoInit[F[_]: Sync: ContextShift: PhotoStorage: Colorization: Fire](blocker: Blocker, config: Config) {
 
   def paths: F[List[String]] = fs2.io.file
     .directoryStream[F](blocker, Path.of(config.photoFolder))
@@ -17,6 +19,6 @@ final class PhotoInit[F[_]: Sync: ContextShift: PhotoStorage: Colorization](bloc
     .compile
     .toList
 
-  def init: F[Unit] = paths.flatTap(x => x.traverse(Colorization[F].colorize(_))).flatMap(PhotoStorage[F].insertMany)
+  def init: F[Unit] = paths.flatTap(x => x.traverse(Colorization[F].colorize(_)).fireAndForget).flatMap(PhotoStorage[F].insertMany)
 
 }
